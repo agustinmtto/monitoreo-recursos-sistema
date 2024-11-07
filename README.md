741022dccf23a47cba337f5f0a18744fcda07f74
agustinmtto <agusmaretto6@gmail.com>
2024-11-07 16:15:02 -0300

    Actualizar README.md y monitor.py

---
+++ README.md
@@ -0,0 +1,45 @@
+# Monitoreo de Recursos del Sistema
+
+Este script permite monitorear el uso de CPU, memoria y disco en el sistema y guarda los datos en un archivo CSV con marcas de tiempo.
+
+## Requisitos
+
+- Python 3.x
+- `psutil`: para instalarlo, ejecuta `pip install psutil`
+
+## Uso
+
+Ejecuta el script en la terminal:
+
+```bash
+python monitor.py
+
+
+Instalación
+Sigue estos pasos para clonar el repositorio y configurar el entorno:
+
+1- Clona el repositorio: 
+git clone https://github.com/agustinmtto/monitoreo-recursos-sistema.git
+
+2- Navega al directorio del proyecto:
+cd monitoreo-recursos-sistema
+
+3- Instala las dependencias necesarias:
+pip install psutil
+
+Uso
+Para ejecutar el script: python monitor.py
+
+
+Ejemplo de Salida
+2024-11-07 15:30:01 | CPU: 23%, Memoria: 45%, Disco: 60%
+2024-11-07 15:30:11 | CPU: 25%, Memoria: 50%, Disco: 63%
+
+Los datos también se guardan en reporte_recursos.csv.
+
+Detener la Ejecución
+Para detener el script, presiona Ctrl + C en la terminal.
+
+
+Autor
+Desarrollado por Agustín Maretto.
--- monitor.py
+++ monitor.py
@@ -2,6 +2,23 @@ import psutil
 import csv
 import time
 
+# Mensaje de bienvenida
+print("Bienvenido al sistema de monitoreo de recursos.")
+print("Este programa monitorea el uso de CPU, memoria y disco.")
+print("Para detener la ejecución, presiona Ctrl + C.")
+
+# Solicitar al usuario que ingrese el intervalo de tiempo
+tiempo = 0
+while tiempo < 1:
+    tiempo = int(input("Ingrese cada cuantos segundos desea monitorear el sistema (mayor o igual a 1 segundo): "))
+    if tiempo < 1:
+        print("El intervalo debe ser mayor o igual a 1 segundo. Intenta de nuevo.")
+
+# Aquí comenzaría el monitoreo con el intervalo que el usuario ha seleccionado
+print(f"Monitoreo comenzado. Intervalo: {tiempo} segundos.")
+
+
+
 # Función para obtener las métricas del sistema
 def obtener_datos():
     # Obtén el porcentaje de uso de CPU
@@ -35,6 +52,6 @@ try:
         print(f"{fecha_hora} | CPU: {cpu}%, Memoria: {memoria}%, Disco: {disco}%")
         guardar_en_csv(cpu, memoria, disco)
         # Espera 10 segundos antes de obtener los datos nuevamente
-        time.sleep(10)
+        time.sleep(tiempo)
 except KeyboardInterrupt:
     print("\nMonitoreo detenido por el usuario.")
